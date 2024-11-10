af30887e4e30fbb9b5b5d43b8fbf34f0ccaec1e0
Edward Kim <itgma1231@gmail.com>
2024-11-09 23:03:11 -0500

    load HTML file/display icon for application

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,11 +1,16 @@
 const { app, BrowserWindow } = require('electron')
 
+let mainWindow
+
 const createMainWindow = () => {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         title: 'ImageShrink',
+        icon: './assets/icons/Icon_256x256.png',
         width: 500,
-        height: 600,       
+        height: 600,
     })
+
+    mainWindow.loadFile('./app/index.html')
 }
 
 app.on('ready', createMainWindow)
