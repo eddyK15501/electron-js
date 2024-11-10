c78295af92765a5141722277b5b2bb153dd465c6
Edward Kim <itgma1231@gmail.com>
2024-11-09 23:20:02 -0500

    set process.env.NODE_ENV to dev/set macOS conventions

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,16 +1,34 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 
-let mainWindow
+process.env.NODE_ENV = 'development';
+
+const inDevMode = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
+
+let mainWindow;
 
 const createMainWindow = () => {
-    mainWindow = new BrowserWindow({
-        title: 'ImageShrink',
-        icon: './assets/icons/Icon_256x256.png',
-        width: 500,
-        height: 600,
-    })
+  mainWindow = new BrowserWindow({
+    title: 'ImageShrink',
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: inDevMode,
+    width: 500,
+    height: 600,
+  });
 
-    mainWindow.loadFile('./app/index.html')
-}
+  mainWindow.loadFile('./app/index.html');
+};
 
-app.on('ready', createMainWindow)
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
+
+app.on('ready', createMainWindow);
